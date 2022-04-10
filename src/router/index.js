e21bff1654fe879ee1b208bc51d0c68d996a29b4
fellowbeard <virgovibe@gmail.com>
2022-04-10 05:33:27 -1000

    initital commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+import PostsIndex from "../views/PostsIndex.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  { path: "/signup", name: "signup", component: SignupView },
+  { path: "/login", name: "login", component: LoginView },
+  { path: "/logout", name: "logout", component: LogoutView },
+  { path: "/posts", name: "posts", component: PostsIndex },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="blogs">
+    <h1>Read All the Things!</h1>
+    <img src="https://munofore.com/wp-content/uploads/2015/10/blogging-cat.jpg" />
+  </div>
+</template>
